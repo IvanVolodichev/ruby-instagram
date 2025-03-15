6bd338a23db30be3212a239b531215eb766c8988
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-15 14:55:02 +0300

    routes without authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+  get "/home", to: "home#index"
+  resources :users do
+    resources :followers, only: [ :index, :create, :destroy ]
+    resources :posts do
+        resources :comments, only: [ :index, :create, :destroy ]
+        resources :likes, only: [ :index, :create, :destroy ]
+      end
+  end
 end
