39996a5743dcae2a303691c39a59ae4905460f51
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-15 17:42:33 +0300

    routes without authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     resources :posts do
         resources :comments, only: [ :index, :create, :destroy ]
         resources :likes, only: [ :index, :create, :destroy ]
-      end
+    end
   end
 end
