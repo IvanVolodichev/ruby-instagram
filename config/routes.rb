6cff2547201138d07c252ca3d92108d808f9ab1a
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-17 13:07:40 +0300

    create route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :users do
     resources :followers, only: [ :index, :create, :destroy ]
     resources :posts do
-        resources :comments, only: [ :index, :create, :destroy ]
-        resources :likes, only: [ :index, :create, :destroy ]
+      resources :comments, only: [ :index, :create, :destroy ]
+      resources :likes, only: [ :index, :create, :destroy ]
     end
   end
 end
