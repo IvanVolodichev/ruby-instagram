d08ee7c0fbb02d9cce5a8d93efccc122e6441232
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-17 10:31:05 +0300

    devise config and create views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/home", to: "home#index"
+  root to: "home#index"
   resources :users do
     resources :followers, only: [ :index, :create, :destroy ]
     resources :posts do
