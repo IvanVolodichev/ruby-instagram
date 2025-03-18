b3cc81e212dfc34a7f91fd8cde07a2c97b656471
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-18 15:41:56 +0300

    crud for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_many :posts
+  devise   :database_authenticatable, :registerable, :validatable
 end
--- db/migrate/20250306185457_create_post_images.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostImages < ActiveRecord::Migration[8.0]
-  def change
-    create_table :post_images do |t|
-      t.references :post, null: false, foreign_key: { on_delete: :cascade }
-      t.string :image_url, null: false, limit: 255
-      t.timestamps
-    end
-  end
-end
