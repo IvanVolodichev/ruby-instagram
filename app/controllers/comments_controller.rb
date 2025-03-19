e3f70b87412f261cbef417ba477f33e7c75512c8
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-19 11:06:05 +0300

    create and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,38 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, :check_owner, only: [ :destroy ]
+
+  def create
+    @comment  = current_user.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to post_path(id: comment_params[:post_id])
+    else
+      redirect_to @comment.post
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      redirect_to post_path(id: comment_params[:post_id])
+    else
+      render "show"
+    end
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def check_owner
+    if @comment.user_id != current_user.id
+      flash[:error] = "У вас нет прав на удаление этого коментария"
+      redirect_to @comment.post
+    end
+  end
+
+  def comment_params
+    params.permit(:text, :post_id)
+  end
 end
